915d23ba8c2ad6a10311a4041785c4c0dae879ab
JHERALD HUREN CACERES APAZA <93649530+JheraldC@users.noreply.github.com>
2022-05-07 17:39:10 -0500

    Update Exercise2.java

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -1,6 +1,6 @@
 package com.example.project;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Exercise2 {
 
@@ -14,15 +14,34 @@ public class Exercise2 {
             for(int i = 0; i < 7; i++){
                 int n = sc.nextInt();
                 a[i] = n;
-            }
-			
+            }		
 			System.out.println(obj.getMenorNumeroSaltos(a));			
 		}
 	}
 
 	public Integer getMenorNumeroSaltos(int a []) {
-
-		// TO DO
-		return -1;
+		
+		//Convertir el arreglo en un ArrayList
+		 ArrayList<Integer> piedras = new ArrayList<Integer>();
+		 for (int i = 0; i < a.length; i++) {
+			piedras.add(a[i]);
+		}
+		 
+		 int ult_posicion = 0, max_salto = 50, saltos = 0;
+		 
+		 while(true) {
+			 for (int i = 0; i < piedras.size(); i++) {
+				if(piedras.get(i) <= (ult_posicion + max_salto)) {} 
+				else {
+					ult_posicion = piedras.get(i-1);		
+					if(Math.abs(piedras.get(i) - ult_posicion) <= max_salto && Math.abs(piedras.get(i) - ult_posicion) >= 0) {
+						saltos++;
+					} else {
+						return -1;
+					}
+				}
+			 }
+			 return saltos+1;
+		 }
 	}
 }
